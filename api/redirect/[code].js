1af04a840ea707684d97f14bbb325a446083f092
Seavhab-heng <seavhab.kh@Gmail.com>
2025-07-10 08:50:08 +0700

    Create [code].js

---
+++ api/redirect/[code].js
@@ -0,0 +1,20 @@
+const fs = require('fs').promises;
+const path = require('path');
+
+module.exports = async (req, res) => {
+  const { code } = req.query;
+  const filePath = path.join(process.cwd(), 'links.json');
+
+  try {
+    const data = await fs.readFile(filePath, 'utf8');
+    const json = JSON.parse(data);
+    const link = json.links.find(l => l.shortCode === code);
+    if (link) {
+      res.redirect(302, link.longUrl);
+    } else {
+      res.status(404).json({ error: 'Link not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Server error' });
+  }
+};
